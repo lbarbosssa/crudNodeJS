f5950819b2d63708198eb78988ac4d14e1c601cf
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2019-09-03 16:32:35 -0300

    Body parser

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-/**
- * 
- */
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
 
 //Template Engine
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
-// BD Connect
-const Sequelize = require('sequelize')
+//BD Connect
 const sequelize = new Sequelize('teste', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql'})
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Rotas
 app.get('/cad', (req, res) => {
     res.render('formulario')
+})
+
+app.post('/add', (req, res) => {
+    res.send('Titulo: ' + req.body.titulo + '<br>Conteudo: ' + req.body.conteudo)
 })
 
 app.listen(8081,() => {
